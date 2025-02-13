001f00e3ae16f29ad2e9acef236e82026061b693
Muhammad Owais <MuhammadOwais03>
2025-02-13 21:57:26 +0500

    updating multer-2

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -2,31 +2,47 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-// const publicDir = path.join(__dirname, "public");
-// if (!fs.existsSync(publicDir)) {
-//   fs.mkdirSync(publicDir);
+// // const publicDir = path.join(__dirname, "public");
+// // if (!fs.existsSync(publicDir)) {
+// //   fs.mkdirSync(publicDir);
+// // }
+
+// const tempDir = path.resolve("/tmp");
+// if (!fs.existsSync(tempDir)) {
+//   fs.mkdirSync(tempDir, { recursive: true });
 // }
 
-const tempDir = path.resolve("/tmp");
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true });
-}
+// const upload = multer({
+  
+//   destination: function (req, file, cb) {
+//     // console.log("Entered Multer", file)
+//     cb(null, tempDir);
+//   },
+//   fileFilter: (req, file, cb) => {
+//     // Only allow PDF files
+//     if (file.mimetype !== "application/pdf") {
+//       return cb(new Error("Only PDF files are allowed"));
+//     }
+//     cb(null, true);
+//   },
+// });
+
+
+const storage = multer.memoryStorage(); // Use memory storage instead of local disk
 
 const upload = multer({
-  
-  destination: function (req, file, cb) {
-    // console.log("Entered Multer", file)
-    cb(null, tempDir);
-  },
+  storage,
   fileFilter: (req, file, cb) => {
-    // Only allow PDF files
     if (file.mimetype !== "application/pdf") {
       return cb(new Error("Only PDF files are allowed"));
     }
     cb(null, true);
   },
 });
+
+export { upload };
+
 
 export { upload };
